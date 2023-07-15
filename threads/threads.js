f222dffecc7047a518a0c6ce8eb235b629c5a093
Faraz Tariq <faraztariq12123@gmail.com>
2023-07-15 23:44:29 +0500

    still working

--- threads/threads.js
+++ threads/threads.js
@@ -1,6 +1,6 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
 import { getAuth, onAuthStateChanged} from "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js";
-import { getFirestore,addDoc, collection,query, orderBy,where, getDocs, deleteDoc, doc } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore.js";
+import { getFirestore,addDoc, collection,query, orderBy,where, getDocs,getDoc, deleteDoc, doc } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore.js";
 
 
 
@@ -22,6 +22,7 @@ const auth = getAuth(app);
 const db = getFirestore(app);
 
 let postContainer = document.getElementById("postContainer");
+
 let createPost = document.getElementById("createPost");
 let post = document.querySelector('.post');
 let user = document.querySelector('.user');
@@ -29,6 +30,8 @@ let userPara = document.querySelector('#userPara');
 let home = document.querySelector('.home');
 
 let currentUser;
+
+
 onAuthStateChanged(auth, (user) => {
   currentUser = user;
   if (currentUser) {
@@ -46,6 +49,8 @@ onAuthStateChanged(auth, (user) => {
     }
   }
 });
+
+const usersCollection = collection(db, "users");
 
 home.addEventListener("click", () => {
   postContainer.style.display = 'block';
@@ -77,12 +82,18 @@ post.addEventListener('click', () => {
       cancelButton: 'swal2-cancel-button-custom'
     },
     preConfirm: async () => {
-      const postInput = Swal.getPopup().querySelector('#postInput');
-      const postCollection = collection(db, 'posts');
-      await addDoc(postCollection, {
-        post: postInput.value,
-        userId: currentUser.uid
-      });
+      try {
+        const postInput = Swal.getPopup().querySelector('#postInput');
+        const postCollection = collection(db, 'posts');
+        await addDoc(postCollection, {
+          post: postInput.value,
+          userId: currentUser.uid,
+          timestamp: new Date() // Add the timestamp field with the current date and time
+        });
+      } catch (error) {
+        console.error('Error adding document:', error);
+        Swal.showValidationMessage('Error adding post. Please try again.'); // Show an error message in the SweetAlert dialog
+      }
     },
     allowOutsideClick: () => !Swal.isLoading(),
     didOpen: () => {
@@ -103,33 +114,42 @@ const deletePost = async (postId) => {
   getDataFromFirestore();
   alert('Post deleted successfully!');
 };
-
 const getDataFromFirestore = async () => {
   const querySnapshot = await getDocs(collection(db, 'posts'));
   postContainer.innerHTML = '';
 
-  querySnapshot.forEach((snapshot) => {
+  querySnapshot.forEach(async (snapshot) => {
     const docId = snapshot.id;
     const data = snapshot.data();
 
+    const userDoc = await getDoc(doc(usersCollection, data.userId)); // Fetch the user document using userId
+    const ownerEmail = userDoc.exists() ? userDoc.data().email : 'Unknown User';
+
     let div = document.createElement('div');
+
+    // let ownerLabel = document.createElement('label');
+    // ownerLabel.textContent = `Owner: ${ownerEmail}`;
+    // div.appendChild(ownerLabel);
+
+    let hr = document.createElement('hr');
+    hr.classList.add("hr");
     let para = document.createElement('p');
+    let span = document.createElement("span");
+    span.classList.add('timeSpan');
+    span.textContent = getPostTime(data.timestamp); // Pass the timestamp field to getPostTime
     para.innerHTML = data.post;
     para.classList.add('myPara');
     div.classList.add('myDiv');
     div.appendChild(para);
 
+    
+
     const postUserId = data.userId;
     if (currentUser && currentUser.uid === postUserId) {
-      // let ownerLabel = document.createElement('label');
-      // ownerLabel.textContent = currentUser.email;
-      // ownerLabel.classList.add('ownerLabel');
-      // div.insertBefore(ownerLabel, para);
-
-      let deleteButton = document.createElement('button');
-      deleteButton.classList.add('delete');
-      deleteButton.textContent = 'Delete Post';
-      div.appendChild(deleteButton);
+      let deleteButton = document.createElement('i');
+deleteButton.classList.add('delete');
+deleteButton.innerHTML = '<i class="fas fa-trash"></i>'; // Replace button text with Font Awesome icon
+div.appendChild(deleteButton);
 
       deleteButton.addEventListener('click', () => {
         const confirmDelete = confirm('Are you sure you want to delete this post?');
@@ -138,7 +158,7 @@ const getDataFromFirestore = async () => {
         }
       });
     }
-
+div.appendChild(span);
     let commentContainer = document.createElement('div');
     commentContainer.classList.add('commentContainer');
     div.appendChild(commentContainer);
@@ -146,15 +166,14 @@ const getDataFromFirestore = async () => {
     let commentRow = document.createElement('div');
     commentRow.classList.add('commentRow');
     div.appendChild(commentRow);
-
+  
     let commentInput = document.createElement('input');
     commentInput.placeholder = 'Enter a comment here';
     commentInput.classList.add('commentInput');
     commentRow.appendChild(commentInput);
 
-    let commentButton = document.createElement('button');
-    commentButton.textContent = 'Add Comment';
-    commentButton.classList.add('commentButton');
+    let commentButton = document.createElement('i');
+    commentButton.innerHTML = '<i class="fas fa-arrow-right"></i>'; // Replace button text with Font Awesome icon
     commentRow.appendChild(commentButton);
 
     commentButton.addEventListener('click', async () => {
@@ -166,32 +185,78 @@ const getDataFromFirestore = async () => {
         userId: currentUser.uid
       });
       commentInput.value = '';
-      getCommentsForPost(docId, commentContainer);
+      getCommentsForPost(docId, commentContainer, div);
       console.log('Comment added to Firestore');
     });
 
-    getCommentsForPost(docId, commentContainer);
-
+    commentInput.value = '';
+    getCommentsForPost(docId, commentContainer, div);
+    console.log('Comment added to Firestore');
+div.appendChild(hr);
     postContainer.insertBefore(div, postContainer.firstChild);
   });
 
   console.log('Data retrieved from Firestore');
+
+
+const getPostTime = (timestamp) => {
+  const postDate = timestamp.toDate(); // Convert the Firestore timestamp to a JavaScript Date object
+  const currentDate = new Date();
+
+  const seconds = Math.floor((currentDate - postDate) / 1000); // Calculate the time difference in seconds
+
+  if (seconds < 60) {
+    return `few seconds`;
+  } else if (seconds < 3600) {
+    const minutes = Math.floor(seconds / 60);
+    return `${minutes}m`;
+  } else if (seconds < 86400) {
+    const hours = Math.floor(seconds / 3600);
+    return `${hours}h`;
+  } else {
+    const days = Math.floor(seconds / 86400);
+    return `${days}d`;
+  }
 };
 
-const getCommentsForPost = async (postId, container) => {
+const getCommentsForPost = async (postId, container, div) => {
   const commentsQuerySnapshot = await getDocs(query(collection(db, 'comments'), where('postId', '==', postId)));
+  
+  // Remove existing comments from the container
   container.innerHTML = '';
 
   commentsQuerySnapshot.forEach((commentSnapshot) => {
     const commentData = commentSnapshot.data();
+    const commentUserId = commentData.userId;
 
     let commentElement = document.createElement('div');
     commentElement.classList.add('comment');
-    commentElement.textContent = commentData.comment;
-
     container.appendChild(commentElement);
-  });
-};
 
-getDataFromFirestore();
-console.log('Data retrieved from Firestore');
+    if (currentUser && currentUser.uid === commentUserId) {
+      let commentDelete = document.createElement('i');
+      commentDelete.classList.add('deleteComment');
+      commentDelete.innerHTML = '<i class="fas fa-trash"></i>'; // Replace button text with Font Awesome icon
+
+      // Add the event listener for comment deletion
+      commentDelete.addEventListener('click', async () => {
+        const confirmDelete = confirm('Are you sure you want to delete this comment?');
+        if (confirmDelete) {
+          await deleteDoc(doc(db, 'comments', commentSnapshot.id));
+          getCommentsForPost(postId, container, div); // Fetch and add the updated comments
+          console.log('Comment deleted successfully!');
+        }
+      });
+
+      commentElement.appendChild(commentDelete);
+    }
+
+    let commentText = document.createElement('span');
+    commentText.textContent = commentData.comment;
+    commentElement.appendChild(commentText);
+  });
+
+  console.log('Data retrieved from Firestore');
+};
+}
+getDataFromFirestore()
