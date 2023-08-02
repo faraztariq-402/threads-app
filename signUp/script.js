e2cc40a6a3ade1acb071d9ea7b45a10b16e52ecc
Faraz Tariq <faraztariq12123@gmail.com>
2023-08-02 22:55:52 +0500

    done

--- signUp/script.js
+++ signUp/script.js
@@ -30,7 +30,7 @@ loginWithGoogle.addEventListener("click", ()=>{
     const user = result.user;
     alert("google login successful")
     console.log("google user created",user)
-    location.href = '../threads/index.html';
+    window.location.href = '../threads/index.html';
   }).catch((error) => {
     // Handle Errors here.
     const errorCode = error.code;
